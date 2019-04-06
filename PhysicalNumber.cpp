3ec1972136aafc6ee6389fb83793b6bcf2cb5a3c
peleg122 <peleg122@gmail.com>
2019-04-06 12:26:38 +0300

    Update PhysicalNumber.cpp

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -5,24 +5,10 @@
 #include "PhysicalNumber.h"
 using namespace std;
 //Positive
-ariel::PhysicalNumber operator+(const ariel::PhysicalNumber &b) {
-    //result is returned after ~|a+b = c (return c)|~
-    return ariel::PhysicalNumber(0, ariel::Unit::HOUR);
-}
 
-ariel::PhysicalNumber ariel::operator+(ariel::PhysicalNumber &a, const ariel::PhysicalNumber &b) {
-    return ariel::PhysicalNumber(0, Unit::KM);
-}
 
-ariel::PhysicalNumber ariel::operator+=(ariel::PhysicalNumber &number, const PhysicalNumber &other) {
-    //+= :
-    //number = number + other
-    return number;
-}
-
-ariel::PhysicalNumber ariel::operator+( PhysicalNumber &a) {
-    //unary +
-    return ariel::PhysicalNumber(0, Unit::HOUR);
+ariel::PhysicalNumber &ariel::operator+=(ariel::PhysicalNumber &b, const ariel::PhysicalNumber &a) {
+    return b;
 }
 
 //negative
@@ -63,13 +49,32 @@ bool ariel::operator!=(const PhysicalNumber &a, const PhysicalNumber &b) {
     return true;
 }
 
-istream &ariel::operator>>(istream &in, ariel::PhysicalNumber &a) {
+istream& ariel::operator>>(istream &in, ariel::PhysicalNumber &a) {
+    //in>>a.data >> a.unit;
     return in;
 }
 
-ostream &ariel::operator<<(ostream &out,const ariel::PhysicalNumber &a) {
+ostream& ariel::operator<<(ostream &out,const ariel::PhysicalNumber &a) {
+    int temp = a.unit;
+    string t ="";
+    switch (temp){
+        case 0: t= "cm";break;
+        case 1: t= "m";break;
+        case 2: t= "km";break;
+        case 3: t= "sec";break;
+        case 4: t= "min";break;
+        case 5: t= "hour";break;
+        case 6: t= "g";break;
+        case 7: t= "kg";break;
+        case 8: t= "ton";break;
+        case 9: t= "bad";break;
+    }
+    out<< a.data << "[" <<t<<"]";
     return out;
 }
+
+
+
 
 //
 
